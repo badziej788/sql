82a7f19ffeae9b72d2b973a38534eb7e00c3e191
badziej788 <badziej788@gmail.com>
2016-05-30 23:41:31 +0200

    Update createak.sql

--- createak.sql
+++ createak.sql
@@ -29,7 +29,7 @@ CREATE TABLE koncert
     godzina_rozpoczecia TIME NOT NULL,
     miejsce VARCHAR(50) NOT NULL,
     miejscowosc VARCHAR(30) NOT NULL,
-    sponsor INT
+    sponsor VARCHAR(30)
 
 );
 -- --------------------------------------------------------
