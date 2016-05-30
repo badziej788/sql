398defb9669586611a296df8c4786e12698b4dff
badziej788 <badziej788@gmail.com>
2016-05-30 14:32:05 +0200

    Update dropak.sql

--- dropak.sql
+++ dropak.sql
@@ -1,6 +1,6 @@
-drop table agecjakoncertowa;
+drop table agecja_koncertowa;
 drop table gatunek;
 drop table koncert;
 drop table support;
 drop table wstep;
-drop table wykonawcy;
+drop table wykonawca;
