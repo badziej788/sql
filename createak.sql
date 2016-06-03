3682740cea21dcc053d7e8303031789d0c67c469
badziej788 <badziej788@gmail.com>
2016-06-03 15:08:28 +0200

    Update createak.sql

--- createak.sql
+++ createak.sql
@@ -6,22 +6,28 @@ drop table koncert;
 drop table support;
 drop table wstep;
 drop table wykonawca;
+-- --------------------------------------------------------
+drop table organizacja;
+drop table wejscie;
+drop table pomaga;
+drop table udzial;
+drop table rodzaj;
+drop table wejscie;
+drop table gra;
 
 
 CREATE TABLE agecja_koncertowa
 (
-    IdAK SERIAL PRIMARY KEY NOT NULL,
-    NIP INT NOT NULL,
+    NIP INT PRIMARY KEY NOT NULL,
     nazwa VARCHAR(30) NOT NULL,
-    przedstawiciel VARCHAR(30) ,
+    przedstawiciel VARCHAR(30) NOT NULL,
     siedziba VARCHAR(30) NOT NULL,
     adres VARCHAR(50) NOT NULL,
     glowny_organizator VARCHAR(50) NOT NULL
 
 );
+
 -- --------------------------------------------------------
-
-
 CREATE TABLE koncert
 (
     idkoncert SERIAL PRIMARY KEY NOT NULL,
@@ -31,13 +37,10 @@ CREATE TABLE koncert
     godzina_rozpoczecia TIME NOT NULL,
     miejsce VARCHAR(50) NOT NULL,
     miejscowosc VARCHAR(30) NOT NULL,
-    sponsor VARCHAR(30)
+    sponsor INT
 
 );
 -- --------------------------------------------------------
-
-
-
 CREATE TABLE wstep
 (
     idwstep SERIAL PRIMARY KEY,
@@ -54,9 +57,6 @@ CREATE TABLE gatunek
 
 );
 -- --------------------------------------------------------
-
-
-
 CREATE TABLE support
 (
     idsupport SERIAL PRIMARY KEY NOT NULL,
@@ -65,9 +65,6 @@ CREATE TABLE support
     wokalista VARCHAR(30)
 
 );
--- --------------------------------------------------------
-
-
 CREATE TABLE wykonawca
 (
     idwykonawca SERIAL PRIMARY KEY NOT NULL,
@@ -76,3 +73,49 @@ CREATE TABLE wykonawca
     data_zalozenia DATE NOT NULL
 
 );
+-- --------------------------------------------------------
+CREATE TABLE pomaga
+(
+    idwykonawca INT,
+    idsupport INT,
+    CONSTRAINT pomaga_support_idsupport_fk FOREIGN KEY (idsupport) REFERENCES support (idsupport),
+    CONSTRAINT pomaga_wykonawca_idwykonawca_fk FOREIGN KEY (idwykonawca) REFERENCES wykonawca (idwykonawca)
+)
+
+CREATE TABLE udzial
+(
+    idkoncert INT,
+    idwykonawca INT,
+    CONSTRAINT udzial_koncert_idkoncert_fk FOREIGN KEY (idkoncert) REFERENCES koncert (idkoncert),
+    CONSTRAINT udzial_wykonawca_idwykonawca_fk FOREIGN KEY (idwykonawca) REFERENCES wykonawca (idwykonawca)
+)
+CREATE TABLE rodzaj
+(
+    idgatunek INT,
+    idkoncert INT,
+    CONSTRAINT rodzaj_gatunek_idgatunek_fk FOREIGN KEY (idgatunek) REFERENCES gatunek (idgatunek),
+    CONSTRAINT rodzaj_koncert_idkoncert_fk FOREIGN KEY (idgatunek) REFERENCES koncert (idkoncert)
+);
+;CREATE TABLE organizacja
+(
+    NIP INT,
+    idkoncert INT,
+    CONSTRAINT organizacja_agecja_koncertowa_NIP_fk FOREIGN KEY (NIP) REFERENCES agecja_koncertowa (NIP),
+    CONSTRAINT organizacja_koncert_idkoncert_fk FOREIGN KEY (idkoncert) REFERENCES koncert (idkoncert)
+);
+CREATE TABLE wejscie
+(
+    idwstep INT,
+    idkoncert INT,
+    CONSTRAINT wejscie_wstep_idwstep_fk FOREIGN KEY (idwstep) REFERENCES wstep (idwstep),
+    CONSTRAINT wejscie_koncert_idkoncert_fk FOREIGN KEY (idwstep) REFERENCES koncert (idkoncert)
+);
+
+CREATE TABLE gra
+(
+    idgatunek INT,
+    idwykonawca INT,
+    CONSTRAINT gra_gatunek_idgatunek_fk FOREIGN KEY (idgatunek) REFERENCES gatunek (idgatunek),
+    CONSTRAINT gra_wykonawca_idwykonawca_fk FOREIGN KEY (idgwykonawca) REFERENCES wykonawca (idwykonawca)
+);
+
